f28dcc828b11bcfbe49f92fc6a48cb8759866b69
hudsonmartins <hudsonbr95@gmail.com>
2021-11-17 10:26:23 -0300

    adding new parameters and creating class name

--- src/segmentation_layer.cpp
+++ src/segmentation_layer.cpp
@@ -31,11 +31,16 @@ void SegmentationLayer::onInitialize()
   nh.param<std::string>("path_ids_str",  path_ids_str, "3"); // zero-indexed ID to clear obstacles
   nh.param<float>("x_range",  x_range, 1.0); // meters to the left/right of the bot to modify costmap
   nh.param<float>("y_range",  y_range, 1.0); // meters in front of the bot to modify costmap
+  nh.param<float>("m_per_pixel",  m_per_pixel, 0.05); // number of meters per pixel
+
+ //the width and height of the image representing a top-down view of the real world
+  nh.param<int>("costmap_height",  costmap_height, 200); 
+  nh.param<int>("costmap_width",  costmap_width, 200); 
 
   parseIntSet(obstacle_ids_str, obstacle_ids);
   parseIntSet(path_ids_str, path_ids);
 
-  parseHomographyConstants(homography_folder);
+  //parseHomographyConstants(homography_folder);
   
   current_ = true;
   new_data = false;
@@ -48,21 +53,20 @@ void SegmentationLayer::onInitialize()
   dsrv_->setCallback(cb);
 
   seg_sub_  = nh.subscribe<sensor_msgs::Image>(segmentation_data, 1, &SegmentationLayer::segNetCb, this);
-
+  std::cout << "reading file " << homography_folder + "/homography.xml" << std::endl;
   cv::FileStorage fs(homography_folder + "/homography.xml", cv::FileStorage::READ);
   fs["homography"] >> h;
   std::cout << "Loaded homography: " << h << std::endl;
   fs.release();
 }
 
-void SegmentationLayer::parseHomographyConstants(const std::string &homography_folder)
+/*void SegmentationLayer::parseHomographyConstants(const std::string &homography_folder)
 {
-  cv::FileStorage fs = cv::FileStorage(homography_folder + "/parameters.yml", cv::FileStorage::READ);
+  cv::FileStorage fs = cv::FileStorage(homography_folder + "/segmentation_layer_parameters.yaml", cv::FileStorage::READ);
   costmap_height = fs["costmap_height"];
   costmap_width = fs["costmap_width"];
   m_per_pixel = fs["m_per_pixel"];
-}
-
+}*/
 
 void SegmentationLayer::parseIntSet(const std::string &raw_list, std::set<int> &int_set)
 {
@@ -82,17 +86,18 @@ void SegmentationLayer::segNetCb(const sensor_msgs::Image::ConstPtr &msg)
 {
   cv_bridge::CvImagePtr cv_ptr;
   try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
-    }
+  {
+    cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+  }
   catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
+  {
+    ROS_ERROR("cv_bridge exception: %s", e.what());
+    return;
+  }
 
   cv::warpPerspective(cv_ptr->image, warped, h, cv::Size(costmap_width, costmap_height));
-  
+  //cv::rotate(warped, warped, cv::ROTATE_90_COUNTERCLOCKWISE);
+  //std::cout << "rows " << warped.rows << " cols " << warped.cols << std::endl;
   int x_range_pixels = x_range/m_per_pixel;
   int y_range_pixels = y_range/m_per_pixel;
   // crop projection to only go a user defined number of meters in x and y direction
@@ -116,7 +121,7 @@ void SegmentationLayer::reconfigureCB(costmap_2d::GenericPluginConfig &config, u
 
   // determines the area of the costmap that is potentially going to be changed
 void SegmentationLayer::updateBounds(double robot_x, double robot_y, double robot_yaw, double* min_x,
-                                           double* min_y, double* max_x, double* max_y)
+                                     double* min_y, double* max_x, double* max_y)
 {
   if (!enabled_)
     return;
@@ -135,26 +140,25 @@ void SegmentationLayer::updateBounds(double robot_x, double robot_y, double robo
   
   for(int y=0; y<overlay.rows; y++){
       for(int x=0; x<overlay.cols; x++){
-	int value = overlay.at<unsigned char>(cv::Point(x,y));
-        if (obstacle_ids.find(value) != obstacle_ids.end()) {
-	  
-	  // shift over point so origin of image is at (0,0)
-	  double mark_x = robot_x + (x - origin.x)*m_per_pixel;
-	  double mark_y = robot_y + (origin.y - y)*m_per_pixel;
+	      int value = overlay.at<unsigned char>(cv::Point(x,y));
+        if(obstacle_ids.find(value) != obstacle_ids.end()) {
+          // shift over point so origin of image is at (0,0)
+          double mark_x = robot_x + (x - origin.x)*m_per_pixel;
+          double mark_y = robot_y + (origin.y - y)*m_per_pixel;
 
-	  unsigned int mx, my;
-	  if(worldToMap(mark_x, mark_y, mx, my)){
-	    setCost(mx, my, LETHAL_OBSTACLE);
-	  }
-	}
-        if (path_ids.find(value) != path_ids.end()) {
-	  double mark_x = robot_x + (x - origin.x)*m_per_pixel;
-	  double mark_y = robot_y + (origin.y - y)*m_per_pixel;
-	  unsigned int mx, my;
-	  if(worldToMap(mark_x, mark_y, mx, my)){
-	    setCost(mx, my, FREE_SPACE);
-	  }
-	}
+          unsigned int mx, my;
+          if(worldToMap(mark_x, mark_y, mx, my)){
+            setCost(mx, my, LETHAL_OBSTACLE);
+          }
+        }
+        if(path_ids.find(value) != path_ids.end()) {
+          double mark_x = robot_x + (x - origin.x)*m_per_pixel;
+          double mark_y = robot_y + (origin.y - y)*m_per_pixel;
+          unsigned int mx, my;
+          if(worldToMap(mark_x, mark_y, mx, my)){
+            setCost(mx, my, FREE_SPACE);
+          }
+        }
       }
   }
 
@@ -167,9 +171,10 @@ void SegmentationLayer::updateBounds(double robot_x, double robot_y, double robo
   new_data = false;
 }
 
-  // actually update the costs within the bounds
-void SegmentationLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i,
-                                          int max_j)
+// actually update the costs within the bounds
+void SegmentationLayer::updateCosts(costmap_2d::Costmap2D& master_grid, 
+                                    int min_i, int min_j, 
+                                    int max_i, int max_j)
 {
   if (!enabled_)
     return;
@@ -180,7 +185,7 @@ void SegmentationLayer::updateCosts(costmap_2d::Costmap2D& master_grid, int min_
     {
       int index = getIndex(i, j);
       if (costmap_[index] == NO_INFORMATION)
-	continue;
+	      continue;
       master_grid.setCost(i, j, costmap_[index]);
     }
   }
